9bf11817b0cca22048e7c30df1a41d9015cf3339
Rune <sungjong5k@naver.com>
2025-07-20 16:12:14 +0900

    change App.tsx file name

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import {StrictMode} from 'react'
 
 import {createRoot} from 'react-dom/client'
 
-import './index.css'
-import App from './app'
+import App from './App'
 
 createRoot(document.querySelector('#root')!).render(
     <StrictMode>
